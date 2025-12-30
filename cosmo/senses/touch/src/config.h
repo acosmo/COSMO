02b766a58ed53efc632975bb65be36f32e60f27c
Abc Abc <health@gmail.com>
2025-12-30 00:17:13 +0000

    COSMO Senses

--- cosmo/senses/touch/src/config.h
+++ cosmo/senses/touch/src/config.h
@@ -1,8 +1,8 @@
 #pragma once
 
 // Wi-Fi credentials
-const char* ssid = "***11";
-const char* password = "***11";
+const char* ssid = "***";
+const char* password = "***";
 
 // COSMO server endpoint
 const char* serverUrl = "http://192.168.1.109:1888";
