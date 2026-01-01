967300b55de91ae82cbb13374d1bc1d12883f47e
Abc Abc <health@gmail.com>
2026-01-01 21:26:35 +0000

    luna added

--- cosmo/FACE/js/speech.js
+++ cosmo/FACE/js/speech.js
@@ -3,6 +3,15 @@ function askCOSMO() {
     fetch(COSMO_HTTP)
         .then(res => console.log("COSMO asked:", res.status))
         .catch(err => console.log("COSMO is not listening; Import cosmo/senses/listen/COSMO_listen.prf.txt into Tasker", err));
+    
+    // ✅ Trigger smile animation
+    if (typeof eyes !== 'undefined') {
+        eyes.express({ type: 'happy' });  // make eyes happy
+    }
+    if (sounds.think) {
+        sounds.think.currentTime = 0;
+        sounds.think.play();
+    }    
 }
 
 // Add click listeners when page loads
